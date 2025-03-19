a6635425f1249bd24f97708b0c5b9d46dc283b8b
LuisDavidOR <luisdavidoporta777@gmail.com>
2025-03-18 23:24:56 -0600

    Modificación de frontend para relacionar con backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Titulo from './components/Titulo'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./views/Login";
+import Inicio from "./views/Inicio";
+import Serviciosviews from "./views/Serviciosviews";
+import './App.css';
 
-const App  = () => {
-  return(
-    <>
-    <Titulo />
-    </>
-  )
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
 
-export default App
+        <Route path="/" element={<Login />} />
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/servicios" element={<Serviciosviews />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/views/Serviciosviews.jsx
@@ -0,0 +1,11 @@
+import Servicios from "../components/servicios/servicios";
+
+const Serviciosviews = () => {
+    return(
+        <>
+            <Servicios titulo="Lista de servicios"></Servicios>
+        </>
+    )
+}
+
+export default Serviciosviews;
